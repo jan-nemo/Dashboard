0ce0a1201dfc9a82f548a9662f0c0093d5c7e612
Micha <micha.kotenko@gmail.com>
2025-07-31 19:43:41 +0300

    1111111

---
+++ src/useWidgetMessagePublisher.ts
@@ -0,0 +1,15 @@
+﻿import {useCallback, useContext} from "react";
+import WidgetConnectorContext from "./WidgetConnectorContext.ts";
+import type {OutboundWidgetMessage} from "./OutboundWidgetMessage.tsx";
+
+type PublishWidgetMessage = (outboundMessage: OutboundWidgetMessage) => void;
+
+export default (): PublishWidgetMessage => {
+  const context = useContext(WidgetConnectorContext);
+  if (!context)
+    throw new Error('useWidgetMessagePublisher must be used within a WidgetConnector');
+
+  const { outboundMessage$ } = context;
+
+  return useCallback((outboundMessage: OutboundWidgetMessage) => outboundMessage$.next(outboundMessage), [outboundMessage$]);
+};
