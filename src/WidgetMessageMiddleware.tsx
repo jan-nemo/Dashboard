b77abba8947eb47f082e7fd699126ce90987ac1a
Micha <micha.kotenko@gmail.com>
2025-07-31 19:15:36 +0300

    111111

--- src/WidgetMessageMiddleware.tsx
+++ src/WidgetMessageMiddleware.tsx
@@ -2,4 +2,4 @@
 import type {InboundWidgetMessage} from "./InboundWidgetMessage.tsx";
 import type {OutboundWidgetMessage} from "./OutboundWidgetMessage.tsx";
 
-export type WidgetMessageMiddleware = (message$: Observable<InboundWidgetMessage>) => Observable<OutboundWidgetMessage>;
+export type WidgetMessageMiddleware = (inboundMessage$: Observable<InboundWidgetMessage>) => Observable<OutboundWidgetMessage>;
